753256c340584d4a90c2a45c5d7872dc1e0db323
ub-victor <victoireushindi371@gmail.com>
2026-09-13 23:41:18 +0200

    Initial Commit

---
+++ src/main/java/com/codewithvictor/store/HomeController.java
@@ -0,0 +1,11 @@
+package com.codewithvictor.store;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String Index() {
+        return "index.html";
+    }
+}
---
+++ src/main/java/com/codewithvictor/store/StoreApplication.java
@@ -0,0 +1,12 @@
+package com.codewithvictor.store;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
