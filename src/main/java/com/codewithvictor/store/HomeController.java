bc29b65e22db26732c0d5a420e8de120fceeff35
ub-victor <victoireushindi371@gmail.com>
2026-09-15 19:02:46 +0200

    Refactor 1+

--- src/main/java/com/codewithvictor/store/HomeController.java
+++ src/main/java/com/codewithvictor/store/HomeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @Value("${spring.application.name}")
+    private String appName;
 
     @RequestMapping("/")
     public String Index() { // this is the method that will handle the request to the root URL
+        System.out.println("Application Name: " + appName); // print the application name to the console
         return "index.html";
     }
 }
