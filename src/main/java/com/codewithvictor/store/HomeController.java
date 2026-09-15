dfba07f7625402f01ef88a4105c6e0f806072d31
ub-victor <victoireushindi371@gmail.com>
2026-09-15 18:58:44 +0200

    Refactor 1+

--- src/main/java/com/codewithvictor/store/HomeController.java
+++ src/main/java/com/codewithvictor/store/HomeController.java
@@ -1,9 +1,12 @@
 package com.codewithvictor.store;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value()
+
     @RequestMapping("/")
     public String Index() { // this is the method that will handle the request to the root URL
         return "index.html";
