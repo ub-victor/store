eb3d2bee0f4893c96c2a72a4d51de12ca8c10f90
ub-victor <victoireushindi371@gmail.com>
2026-09-15 09:48:17 +0200

    Refactor 1+

--- src/main/java/com/codewithvictor/store/StoreApplication.java
+++ src/main/java/com/codewithvictor/store/StoreApplication.java
@@ -1,12 +1,13 @@
 package com.codewithvictor.store;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
 
 }
